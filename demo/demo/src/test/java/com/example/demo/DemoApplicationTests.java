fbe66a138e955a1302975a565108e25e7db484ae
benjiiii <BENJ0045@E.NTU.EDU.SG>
2023-12-06 23:19:05 +0800

    Commit to main.
    Commit to main.

--- demo/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,12 +2,17 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void testAddition() {
+        int a = 7;
+        int b = 10;
+        int expected = 17;
+        int actual = a + b;
+        assertEquals(expected, actual, "5 + 10 should equal 15");
+    }
 }

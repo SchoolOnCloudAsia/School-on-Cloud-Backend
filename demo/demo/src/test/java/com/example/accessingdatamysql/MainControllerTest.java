95b4309fdc34f264d2ee73ba8449273f6fea05d2
benjiiii <BENJ0045@E.NTU.EDU.SG>
2023-12-06 11:18:37 +0800

    Commit to main.
    Commit to main.

--- demo/demo/src/test/java/com/example/accessingdatamysql/MainControllerTest.java
+++ demo/demo/src/test/java/com/example/accessingdatamysql/MainControllerTest.java
@@ -2,82 +2,83 @@ package com.example.accessingdatamysql;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
 import java.util.Arrays;
 import java.util.List;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 public class MainControllerTest {
-
-    private MockMvc mockMvc;
 
     @Mock
     private UserRepository userRepository;
 
-    @InjectMocks
     private MainController mainController;
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(mainController).build();
+        mainController = new MainController(userRepository);
     }
 
     @Test
-    public void testAddNewUser() throws Exception {
-        User user = new User();
-        user.setName("Test");
-        user.setEmail("test@test.com");
-
-        when(userRepository.save(any(User.class))).thenReturn(user);
-
-        mockMvc.perform(post("/demo/add")
-                .contentType(MediaType.APPLICATION_JSON)
-                .param("name", "Test")
-                .param("email", "test@test.com"))
-                .andExpect(status().isOk());
-
-        verify(userRepository, times(1)).save(any(User.class));
-    }
-
-    @Test
-    public void testGetAllUsers() throws Exception {
+    public void testAddUsers() {
+        // Arrange
         User user1 = new User();
-        user1.setName("Test1");
-        user1.setEmail("test1@test.com");
+        user1.setName("User1");
+        user1.setEmail("user1@example.com");
 
         User user2 = new User();
-        user2.setName("Test2");
-        user2.setEmail("test2@test.com");
+        user2.setName("User2");
+        user2.setEmail("user2@example.com");
 
-        List<User> users = Arrays.asList(user1, user2);
+        when(userRepository.save(user1)).thenReturn(user1);
+        when(userRepository.save(user2)).thenReturn(user2);
 
-        when(userRepository.findAll()).thenReturn(users);
+        // Act
+        String response1 = mainController.addNewUser(user1.getName(), user1.getEmail());
+        String response2 = mainController.addNewUser(user2.getName(), user2.getEmail());
 
-        mockMvc.perform(get("/demo/all")
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
-
-        verify(userRepository, times(1)).findAll();
+        // Assert
+        assertEquals("Saved", response1);
+        assertEquals("Saved", response2);
     }
 
     @Test
-    public void testDeleteUser() throws Exception {
-        mockMvc.perform(delete("/{id}", 1))
-               .andExpect(status().isOk())
-               .andExpect(content().string("Deleted"));
+    public void testGetUsers() {
+        // Arrange
+        User user1 = new User();
+        user1.setName("User1");
+        user1.setEmail("user1@example.com");
+
+        User user2 = new User();
+        user2.setName("User2");
+        user2.setEmail("user2@example.com");
+
+        List<User> users = Arrays.asList(user1, user2);
+        when(userRepository.findAll()).thenReturn(users);
+
+        // Act
+        Iterable<User> responseUsers = mainController.getAllUsers();
+
+        // Assert
+        assertEquals(users.size(), ((List<User>) responseUsers).size());
+    }
+
+    @Test
+    public void testDeleteUser() {
+        // Arrange
+        Integer id = 1;
+        doNothing().when(userRepository).deleteById(id);
+
+        // Act
+        String response = mainController.deleteUser(id);
+
+        // Assert
+        assertEquals("Deleted", response);
     }
 }

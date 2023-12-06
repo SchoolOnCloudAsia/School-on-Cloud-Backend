70af25d051eecaa6898ffae02f38277c1fc65230
benjiiii <BENJ0045@E.NTU.EDU.SG>
2023-12-06 12:25:00 +0800

    Commit to main.
    Commit to main.

--- demo/demo/src/main/java/com/example/accessingdatamysql/MainController.java
+++ demo/demo/src/main/java/com/example/accessingdatamysql/MainController.java
@@ -1,46 +1,40 @@
 package com.example.accessingdatamysql;
 
-// The import org.springframework.beans.factory.annotation.Autowired is not used
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller // This means that this class is a Controller
-@RequestMapping(path="/demo") // This means URL's start with /demo (after Application path)
+@Controller // This annotation indicates that this class serves as a web controller
+@RequestMapping(path="/demo") // This annotation maps HTTP requests to the path "/demo" onto methods in this controller
 public class MainController {
   private UserRepository userRepository;
 
+  // Constructor for the MainController class, UserRepository is injected as a dependency
   public MainController(UserRepository userRepository) {
     this.userRepository = userRepository;
   }
 
-  @PostMapping(path="/add") // Map ONLY POST Requests
-  public @ResponseBody String addNewUser (@RequestParam String name
-      , @RequestParam String email) {
-    // @ResponseBody means the returned String is the response, not a view name
-    // @RequestParam means it is a parameter from the GET or POST request
+  @PostMapping(path="/add") // This annotation ensures that HTTP POST requests to "/demo/add" are mapped to this method
+  public @ResponseBody String addNewUser (@RequestParam String name, @RequestParam String email) {
+    // @ResponseBody annotation tells a controller that the object returned is automatically serialized into JSON and passed back into the HttpResponse object
+    // @RequestParam binds the value of the query string parameter 'name' and 'email' into the respective method parameters 'name' and 'email'
 
     User n = new User();
     n.setName(name);
     n.setEmail(email);
-    userRepository.save(n);
-    return "Saved";
+    userRepository.save(n); // Save the user to the database
+    return "Saved"; // Return a confirmation message
   }
 
-  @GetMapping(path="/all")
+  @GetMapping(path="/all") // This annotation ensures that HTTP GET requests to "/demo/all" are mapped to this method
   public @ResponseBody Iterable<User> getAllUsers() {
-    // This returns a JSON or XML with the users
-    return userRepository.findAll();
+    // This method returns a JSON or XML with all users
+    return userRepository.findAll(); // Retrieve all users from the database
   }
 
-  @DeleteMapping(path="/{id}")
+  @DeleteMapping(path="/{id}") // This annotation ensures that HTTP DELETE requests to "/demo/{id}" are mapped to this method
   public @ResponseBody String deleteUser(@PathVariable Integer id) {
-      userRepository.deleteById(id);
-      return "Deleted";
+    // @PathVariable annotation indicates that a method parameter should be bound to a URI template variable
+    userRepository.deleteById(id); // Delete the user with the provided ID from the database
+    return "Deleted"; // Return a confirmation message
   }
 }
--- demo/demo/src/test/java/com/example/accessingdatamysql/MainControllerTest.java
+++ demo/demo/src/test/java/com/example/accessingdatamysql/MainControllerTest.java
@@ -14,18 +14,18 @@ import static org.mockito.Mockito.when;
 
 public class MainControllerTest {
 
-    @Mock
+    @Mock // This annotation is used to create a mock implementation of UserRepository
     private UserRepository userRepository;
 
     private MainController mainController;
 
-    @BeforeEach
+    @BeforeEach // This annotation defines the method to be executed before each test
     public void setup() {
-        MockitoAnnotations.openMocks(this);
-        mainController = new MainController(userRepository);
+        MockitoAnnotations.openMocks(this); // Initializes objects annotated with @Mock
+        mainController = new MainController(userRepository); // Inject the mock into the MainController
     }
 
-    @Test
+    @Test // This annotation indicates that the public void method to which it is attached can be run as a test case
     public void testAddUsers() {
         // Arrange
         User user1 = new User();
@@ -36,6 +36,7 @@ public class MainControllerTest {
         user2.setName("User2");
         user2.setEmail("user2@example.com");
 
+        // Define the behavior of the mock object
         when(userRepository.save(user1)).thenReturn(user1);
         when(userRepository.save(user2)).thenReturn(user2);
 
@@ -44,11 +45,11 @@ public class MainControllerTest {
         String response2 = mainController.addNewUser(user2.getName(), user2.getEmail());
 
         // Assert
-        assertEquals("Saved", response1);
-        assertEquals("Saved", response2);
+        assertEquals("Saved", response1); // Check if the method returns "Saved" for user1
+        assertEquals("Saved", response2); // Check if the method returns "Saved" for user2
     }
 
-    @Test
+    @Test // This annotation indicates that the public void method to which it is attached can be run as a test case
     public void testGetUsers() {
         // Arrange
         User user1 = new User();
@@ -60,25 +61,25 @@ public class MainControllerTest {
         user2.setEmail("user2@example.com");
 
         List<User> users = Arrays.asList(user1, user2);
-        when(userRepository.findAll()).thenReturn(users);
+        when(userRepository.findAll()).thenReturn(users); // Define the behavior of the mock object
 
         // Act
-        Iterable<User> responseUsers = mainController.getAllUsers();
+        Iterable<User> responseUsers = mainController.getAllUsers(); // Call the method to be tested
 
         // Assert
-        assertEquals(users.size(), ((List<User>) responseUsers).size());
+        assertEquals(users.size(), ((List<User>) responseUsers).size()); // Check if the method returns the correct number of users
     }
 
-    @Test
+    @Test // This annotation indicates that the public void method to which it is attached can be run as a test case
     public void testDeleteUser() {
         // Arrange
         Integer id = 1;
-        doNothing().when(userRepository).deleteById(id);
+        doNothing().when(userRepository).deleteById(id); // Define the behavior of the mock object
 
         // Act
-        String response = mainController.deleteUser(id);
+        String response = mainController.deleteUser(id); // Call the method to be tested
 
         // Assert
-        assertEquals("Deleted", response);
+        assertEquals("Deleted", response); // Check if the method returns "Deleted"
     }
 }

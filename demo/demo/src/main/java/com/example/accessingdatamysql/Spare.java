a03e3949e0c47004a6c9bfc793cd76ff643405ec
benjiiii <BENJ0045@E.NTU.EDU.SG>
2023-12-08 20:38:12 +0800

    Commit to main.
    Commit to main.

--- demo/demo/src/main/java/com/example/accessingdatamysql/Spare.java
+++ demo/demo/src/main/java/com/example/accessingdatamysql/Spare.java
@@ -3,16 +3,18 @@ package com.example.accessingdatamysql;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.jaas.JaasAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.ArrayList;
 
 @Configuration
-public class WebSecurityConfig {
+public class WebSecurityConfig extends AbstractHttpConfigurer<WebSecurityConfig, HttpSecurity> {
 
     private final UserRepository userRepository;
 
@@ -45,7 +47,13 @@ public class WebSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Autowired
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        JaasAuthenticationProvider jwtAuthenticationProvider = new JaasAuthenticationProvider();
+        http.authenticationProvider(jwtAuthenticationProvider)
+            .httpBasic(AbstractHttpConfigurer::disable);
+    }
+
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
             .ldapAuthentication()
@@ -55,7 +63,7 @@ public class WebSecurityConfig {
             .url("ldap://localhost:8389/dc=springframework,dc=org")
             .and()
             .passwordCompare()
-            .passwordEncoder(new BCryptPasswordEncoder())
+            .passwordEncoder(passwordEncoder())
             .passwordAttribute("userPassword");
     }
 }

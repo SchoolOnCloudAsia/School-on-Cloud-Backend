a37c120e002f5e082e190b73bcba107c3db6eb80
benjiiii <BENJ0045@E.NTU.EDU.SG>
2023-12-20 08:49:00 +0800

    Commit to main.
    Commit to main.

--- demo/demo/src/main/java/com/example/accessingdatamysql/UserRepository.java
+++ demo/demo/src/main/java/com/example/accessingdatamysql/UserRepository.java
@@ -1,11 +1,12 @@
 package com.example.accessingdatamysql;
 
+import java.util.Optional; // Import the correct Optional class
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
   User findByName(String name); // Method to find a user by name
 
-Optional<User> findById(Long id);
+  Optional<User> findById(Long id); // Fix the Optional type
 
 }

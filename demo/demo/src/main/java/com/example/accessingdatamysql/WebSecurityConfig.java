ff5075b9289af3db0b01d3f699bf2911ae561be4
benjiiii <BENJ0045@E.NTU.EDU.SG>
2024-01-12 18:53:05 +0800

    Updated WebSecurityConfig.java class.
    Updated WebSecurityConfig.java class.

--- demo/demo/src/main/java/com/example/accessingdatamysql/WebSecurityConfig.java
+++ demo/demo/src/main/java/com/example/accessingdatamysql/WebSecurityConfig.java
@@ -6,26 +6,24 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+    private final UserRepository userRepository;
+
+    public WebSecurityConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Bean
     public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User
-                .withUsername("user")
-                .password(passwordEncoder().encode("password"))
-                .roles("USER")
-                .build());
-        return manager;
+        return new CustomUserDetailsService(userRepository);
     }
 
     @Bean

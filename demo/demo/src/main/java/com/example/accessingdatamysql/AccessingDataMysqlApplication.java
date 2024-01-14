25366798edea3d2fbbe1c3b902d80e458c254bcf
benjiiii <BENJ0045@E.NTU.EDU.SG>
2024-01-14 11:11:54 +0800

    Revert "Upgrade Spring Boot From v3.1.6 to v3.1.7."
    This reverts commit 95da2b77df038652e9cc9298834dbfeba166b395.

--- demo/demo/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
+++ demo/demo/src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AccessingDataMySQLApplication {
+public class AccessingDataMysqlApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(AccessingDataMySQLApplication.class, args);
+    SpringApplication.run(AccessingDataMysqlApplication.class, args);
   }
 
 }

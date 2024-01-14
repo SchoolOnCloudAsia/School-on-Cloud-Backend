56bd2ff129adf20d2bcc36a3e6204a5b4fec2f59
benjiiii <BENJ0045@E.NTU.EDU.SG>
2024-01-14 11:03:02 +0800

    Updated SecurityConfig.java.  Edited imports.
    Updated SecurityConfig.java.  Edited imports.

--- demo/demo/src/main/java/com/example/accessingdatamysql/SecurityConfig.java
+++ demo/demo/src/main/java/com/example/accessingdatamysql/SecurityConfig.java
@@ -1,14 +1,16 @@
 package com.example.accessingdatamysql;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;

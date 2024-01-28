ba7dcc5f74cc1de5c8d1b4a7ee5f3ea8f7c876b3
benjiiii <BENJ0045@E.NTU.EDU.SG>
2024-01-28 19:42:49 +0800

    Updated UserRepository.java Class.
    Updated UserRepository.java Class.

--- demo/demo/src/main/java/com/example/accessingdatamysql/UserRepository.java
+++ demo/demo/src/main/java/com/example/accessingdatamysql/UserRepository.java
@@ -2,6 +2,9 @@ package com.example.accessingdatamysql;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
-  User findByUserID(String userID);
+// The UserRepository interface extends CrudRepository which provides methods for CRUD operations.
+// The type of entity and ID that it works with, User and String respectively, are specified in the generic parameters on CrudRepository.
+public interface UserRepository extends CrudRepository<User, String> {
+  // Method to find a User entity by its UserID.
+  User findByUserID(String UserID);
 }
